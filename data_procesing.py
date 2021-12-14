938d37677b536328e2c0bd5525b17a89e309a53b
srinisuryadevara <suryadevara.srinivas@gmail.com>
2021-12-14 08:03:54 +0530

    Added new function sum

--- data_procesing.py
+++ data_procesing.py
@@ -1,2 +1,9 @@
 
-print("Fradu Detction Repo")
+print("Adding all the files for Data processing")
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    retrun c
+
+print("new functions added to find sum of two numbers",c)
