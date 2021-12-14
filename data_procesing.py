51c1cce73101d9ba25185db33beffe4eb68777e1
srinisuryadevara <suryadevara.srinivas@gmail.com>
2021-12-14 08:07:45 +0530

    Revert "Added new function sum" reverted sum function
    This reverts commit 938d37677b536328e2c0bd5525b17a89e309a53b.

--- data_procesing.py
+++ data_procesing.py
@@ -1,9 +1,2 @@
 
-print("Adding all the files for Data processing")
-a=5
-b=7
-def sum(a,b):
-    c=a+b
-    retrun c
-
-print("new functions added to find sum of two numbers",c)
+print("Fradu Detction Repo")
