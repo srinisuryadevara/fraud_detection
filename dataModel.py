7e811757ef56d52ecd5c63b72dec07766d1d1bb7
SriniSurya <suryadevara.srinivas@gmail.com>
2022-09-06 22:23:30 +0800

    Revert "removed test code"
    This reverts commit 0bfb9f43d429ff00f39fb0e84b1d42f9ea0b26d3.

--- dataModel.py
+++ dataModel.py
@@ -2,4 +2,13 @@ print("This Code Demonstrates Various Models Tested and Finalized good working M
  def model1():
     print("Inside Model1")
 
- 
+ def model2:
+   print("Inside Model2")
+
+ def model3:
+   print("Inside Model3")
+
+print("Finlizinged Model2")
+
+model2
+
